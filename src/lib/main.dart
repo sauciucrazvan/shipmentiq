d5f9044d7c883643a5ebd503ec5156367b33bb84
Razvan Sauciuc <razvansau9@gmail.com>
2023-12-16 18:09:44 +0200

    Added the flutter project to the source folder

---
+++ src/lib/main.dart
@@ -0,0 +1,15 @@
+/*
+
+  ShipmentIQ
+  An upcoming inventory management application built for businesses that can be easily adapted to different scenarios.
+
+  Credits:
+  Razvan Sauciuc - main developer
+
+*/
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Placeholder());
+}
